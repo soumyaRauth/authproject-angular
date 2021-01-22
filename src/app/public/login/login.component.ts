83754ce18081829b953c8574ef0851381d41fe9f
soumyaRauth <shoummorauth@gmail.com>
2021-01-22 15:30:07 +0600

    login authentication in progress

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ɵangular_packages_platform_browser_platform_browser_d } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+ 
+  form:FormGroup 
+  
+  constructor(private fb:FormBuilder, private http:HttpClient) { }
 
   ngOnInit(): void {
+
+    this.form=this.fb.group({
+      email:'',
+      password:''
+    })
+  }
+
+
+  submit(){
+    const formData=this.form.getRawValue();
+
+    const data={
+      username:formData.email,
+      password:formData.password,
+      grant_type:'password',
+      client_id:4,
+      client_secret:'gOa57dvKkswhFiua54YKP4ahP8X0gUDGsmFwFUeb',
+      scope:'*'
+    }
+    
+    this.http.post('http://localhost:8000/oauth/token',data).subscribe(data=>{
+      console.log('successfully logged in');
+      console.log(data);
+      
+    },err=>{
+      console.log('Error');
+      console.log(err);
+      
+    })
   }
 
 }
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,14 +4,15 @@ import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
-
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent,PublicComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class PublicModule { }
