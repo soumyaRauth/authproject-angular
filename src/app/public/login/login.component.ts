450983b8cb9a87c61d607d2ae4efcc671ef9b339
soumyaRauth <shoummorauth@gmail.com>
2021-01-23 03:58:49 +0600

    token received from the login api

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,49 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ɵangular_packages_platform_browser_platform_browser_d } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
- 
-  form:FormGroup 
-  
-  constructor(private fb:FormBuilder, private http:HttpClient) { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private route: Router
+  ) {}
 
   ngOnInit(): void {
-
-    this.form=this.fb.group({
-      email:'',
-      password:''
-    })
+    this.form = this.fb.group({
+      email: '',
+      password: '',
+    });
   }
 
+  submit() {
+    const formData = this.form.getRawValue();
 
-  submit(){
-    const formData=this.form.getRawValue();
+    const data = {
+      username: formData.email,
+      password: formData.password,
+      grant_type: 'password',
+      client_id: 6,
+      client_secret: 'Pf4sYnipZI3pMtZuF4oks8MzD4TikEX92XftEvby',
+      scope: '*',
+    };
 
-    const data={
-      username:formData.email,
-      password:formData.password,
-      grant_type:'password',
-      client_id:4,
-      client_secret:'gOa57dvKkswhFiua54YKP4ahP8X0gUDGsmFwFUeb',
-      scope:'*'
-    }
-    
-    this.http.post('http://localhost:8000/oauth/token',data).subscribe(data=>{
-      console.log('successfully logged in');
-      console.log(data);
-      
-    },err=>{
-      console.log('Error');
-      console.log(err);
-      
-    })
+    console.log(data);
+    this.http.post('http://localhost:8000/oauth/token', data).subscribe(
+      (result) => {
+        if (result['access_token']) {
+          localStorage.setItem('token', result['access_token']);
+          console.log(result);
+          this.route.navigate(['/secure']);
+        } else {
+          console.log(result);
+        }
+      },
+      (error) => {
+        console.log('Error');
+        console.log(error);
+      }
+    );
   }
-
 }
